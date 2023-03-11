5dc296a82ffdb3be25880bf9a553ce10e71f38c4
mjax <matt.d.jax@gmail.com>
2023-03-11 09:48:40 -0500

    installed react router

--- client/app.js
+++ client/app.js
@@ -1,10 +1,9 @@
 import Login from './login'
-import React from 'react';
 export function App() {
     return (
         <div>
             <h1>LUNCHRUNNING</h1>
-            <Login/>
+            // <Login/>
         </div>
     )
 }
--- client/index.js
+++ client/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
+
 // const appElement = document.getElementById('root');
 // ReactDOM.render(<App/>, appElement);
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
-root.render(<App/>)
+root.render(
+    <RouterProvider router={router} />
+)
