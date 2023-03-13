01184c9b10179a7c22d6658b13726b5498bb4962
Molly Greene <mollycgreene@gmail.com>
2023-03-13 13:48:49 -0400

    tidied up notes and logs

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -4,15 +4,12 @@ const createError = require("../createError");
 const postController = {};
 
 // creates new post and stores on res.locals.post
-// how does this then go int othe database? 
 postController.createPost = async (req, res, next) => {
     try {
-        //const post = {test: 'create post'}
-        console.log(req.body);
         const post = await Post.create({
-            placeId : req.body.placeId, // <-- we're pretty sure the schema is not enforced and this can be a string
-            expirationTime: req.body.expirationTime, // <-- when they're going (check w frontend about entering date)
-            owner: req.body.owner, //<-- connects to current user by ID (won't need frontend field)
+            placeId : req.body.placeId, 
+            expirationTime: req.body.expirationTime, 
+            owner: req.body.owner, 
         })
         res.locals.post = post;
         next();
@@ -30,7 +27,6 @@ postController.createPost = async (req, res, next) => {
 // returns to client on res.locals.posts
 postController.getPosts = async (req, res, next) => {
     try {
-        //allPosts = {test: 'get posts'}
         const allPosts = await Post.find({})
         res.locals.posts = allPosts;
         next();
