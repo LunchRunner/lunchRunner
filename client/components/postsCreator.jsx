1ebf09f928d23c76f85cf47c51cdd81bb2c4276b
mjax <matt.d.jax@gmail.com>
2023-03-13 15:59:30 -0400

    posts are being created and showing in display

--- client/components/postsCreator.jsx
+++ client/components/postsCreator.jsx
@@ -1,45 +1,50 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
-import { incrementPostTotal } from "../redux/postSlice";
+import { addPost } from "../redux/postSlice";
 
 export default function PostsCreator() {
-   const dispatch = useDispatch();
-   const total = useSelector((state) => state.post.postTotal);
-   const [whereInput, setWhereInput] = useState('');
-   const [whenInput, setWhenInput] = useState('');
-   useEffect(()=> {
+  const dispatch = useDispatch();
+  const [whereInput, setWhereInput] = useState('');
+  const [whenInput, setWhenInput] = useState('');
 
-   },[])
+  function postButtonClick() {
+    const testName = 'test User';
+    const body = { placeId: whereInput, expirationTime: whenInput, owner: testName }
 
-   function postButtonClick() {
-      const testName = 'no name yet';
-      dispatch(incrementPostTotal())
+    dispatch(addPost(body))
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json')
 
-      // Posts.create({});
-      // const requestOptions = {method: "POST", body: {placeId: }}
-      fetch('http://localhost:3000/posts', requestOptions)
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body)
+    }
+    fetch('/posts', requestOptions)
+      .then(res => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err))
 
-      setWhereInput('');
-      setWhenInput('');
-   }
+    setWhereInput('');
+    setWhenInput('');
+  }
 
-    return (
-        <div className="postBox">
-         <div>postHere</div>
-         <div>{total}</div>
-
-           <form className="post">
-              <input value={whereInput} type="text" onChange={(e) => {
-               setWhereInput(e.target.value);
-            }} name="wherePost" placeholder="Where are you going?"/>
-              <input value={whenInput} type="text" onChange={(e)=> {
-               setWhenInput(e.target.value)
-            }} name= "whenPost" placeholder="What time?"/>
-              <button className="postButton" onClick = {(e) => {
-               e.preventDefault()
-               postButtonClick();
-            }}>Post</button>
-           </form>
-        </div>
-    )
+  return (
+    <div className="postBox">
+      <form className="post">
+        <input value={whereInput} type="text" onChange={(e) => {
+          setWhereInput(e.target.value);
+        }} name="wherePost" placeholder="Where are you going?" />
+        <input value={whenInput} type="text" onChange={(e) => {
+          setWhenInput(e.target.value)
+        }} name="whenPost" placeholder="What time?" />
+        <button className="postButton" onClick={(e) => {
+          e.preventDefault()
+          postButtonClick();
+        }}>Post</button>
+      </form>
+    </div>
+  )
 }
--- client/redux/postSlice.js
+++ client/redux/postSlice.js
@@ -2,19 +2,24 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
 	postTotal: 0,
+	posts: [],
 }
 
 export const postSlice = createSlice({
 	name: 'post',
 	initialState,
 	reducers: {
-		incrementPostTotal: (state)=> {
+		addPost: (state, action)=> {
 			state.postTotal++;
+			state.posts.push(action.payload);
+		},
+		setInitialPosts: (state, action) => {
+			state.posts = action.payload; 
+			state.postTotal = action.payload.length;
 		}
-
 	}
 
 })
 
-export const { incrementPostTotal } = postSlice.actions;
+export const { addPost, setInitialPosts } = postSlice.actions;
 export default postSlice.reducer;
