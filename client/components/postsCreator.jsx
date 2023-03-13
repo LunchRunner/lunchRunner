913a987d9093eac62f27a3877c8c10abda7507f5
mjax <matt.d.jax@gmail.com>
2023-03-13 13:23:40 -0400

    post page changes

--- client/components/postsCreator.jsx
+++ client/components/postsCreator.jsx
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { incrementPostTotal } from "../redux/postSlice";
+
 export default function PostsCreator() {
+   const dispatch = useDispatch();
+   const total = useSelector((state) => state.post.postTotal);
+   const [whereInput, setWhereInput] = useState('');
+   const [whenInput, setWhenInput] = useState('');
+   useEffect(()=> {
+
+   },[])
+
+   function postButtonClick() {
+      const testName = 'no name yet';
+      dispatch(incrementPostTotal())
+
+      // Posts.create({});
+      // const requestOptions = {method: "POST", body: {placeId: }}
+      fetch('http://localhost:3000/posts', requestOptions)
+
+      setWhereInput('');
+      setWhenInput('');
+   }
+
     return (
         <div className="postBox">
+         <div>postHere</div>
+         <div>{total}</div>
+
            <form className="post">
-              <input type="text" name="wherePost" placeholder="Where are you going?"/>
-              <input type="text" name= "whenPost" placeholder="What time?"/>
-              <button className="postButton">Post</button>
+              <input value={whereInput} type="text" onChange={(e) => {
+               setWhereInput(e.target.value);
+            }} name="wherePost" placeholder="Where are you going?"/>
+              <input value={whenInput} type="text" onChange={(e)=> {
+               setWhenInput(e.target.value)
+            }} name= "whenPost" placeholder="What time?"/>
+              <button className="postButton" onClick = {(e) => {
+               e.preventDefault()
+               postButtonClick();
+            }}>Post</button>
            </form>
         </div>
     )
--- client/components/postsDisplay.jsx
+++ client/components/postsDisplay.jsx
@@ -1,8 +1,42 @@
-import { useEffect } from "react";
+import { process } from "ipaddr.js";
+import { useEffect, useState } from "react";
 
+
+const dummyData = [{
+   who: 'test',
+   where: 'chipotle',
+   when: '10 mins',
+},
+{
+   who: 'test2',
+   where: 'dumplings',
+   when: '15 mins'
+}];
 export default function() {
+   const [posts, setPosts] = useState([]);
 
-   useEffect(()=>{
+   useEffect(() => {
       //fetch from database posts 
-   },[])
+      // const data = fetch('http://localhost:3000/posts')
+      // console.log(data)
+   }, [])
+
+   function listPosts(posts) {
+      return dummyData.map((post, i) => {
+         return (
+            <li key={`unique${i}`}>
+               {post.username}
+            </li>
+         )
+      })
+   }
+
+   return (
+      <div>
+         <h2>Lunchrunner Postings</h2>
+         <ul>
+            {listPosts(posts)}
+         </ul>
+      </div>
+   )
 }
